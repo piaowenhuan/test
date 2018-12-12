57db9070c6043b94d693fdcbd30febdfb8a118d6
piaowenhuan <45728110+piaowenhuan@users.noreply.github.com>
2018-12-12 20:12:36 +0800

    添加项目文件。

---
+++ MyWebApplication/Data/WebApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyWebApplication.Models
+{
+    public class WebApplicationContext : DbContext
+    {
+        public WebApplicationContext (DbContextOptions<WebApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyWebApplication.Models.Movie> Movie { get; set; }
+    }
+}
